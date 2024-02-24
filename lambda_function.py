496c92066ae78f9d589fb63f558cdd6181b71253
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2024-02-25 02:04:21 +0800

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -28,12 +28,11 @@ def lambda_handler(event, context):
                 'statusCode': 401,
                 'body': 'Bad signature'
             }
-        # https://docs.github.com/en/developers/webhooks-and-events/webhook-events-and-payloads#star
+        # Update repo name & description
         payload = json.loads(body)
         repository = payload['repository']
         repository_url = repository['url']
         stars = repository['stargazers_count']
-
         new_name = f"This-Repo-Has-{stars}-Star{'s' if stars != 1 else ''}"
         new_description = f"Thanks for stopping by! This repository now has {stars} star{'s' if stars != 1 else ''}~🌟🌟🌟"
         headers = {'Authorization': f"Bearer {GITHUB_TOKEN}"}
@@ -42,15 +41,26 @@ def lambda_handler(event, context):
             'description': new_description
         }
         requests.patch(repository_url, headers=headers, json=data)
-
+        # Update title
         readme_url = repository_url + '/contents/README.md'
         res = requests.get(readme_url, headers=headers)
+        sha = res.json()['sha']
         content = base64.b64decode(res.json()['content']).decode()
-        lines = content.split('\n\n')
+        lines = content.split('\n\n')[:3]
         lines[0] = f"# It's True 💗 This Repo Has {stars} Star{'s' if stars != 1 else ''}!"
+        # Update stargazers
+        stargazers_url = repository_url + "/stargazers"
+        res = requests.get(stargazers_url, headers={'Accept': 'application/vnd.github.v3.star+json', 'Authorization': f"Bearer {GITHUB_TOKEN}"}).json()
+        lines.append("下表记录了Star了此项目的用户们:")
+        table = "| No. | Avatar | Username | Starred At |\n" \
+                "| :---: | :---: | :---: | :---: |"
+        total = len(res)
+        for i, info in enumerate(res[::-1], 1):
+            user = info['user']
+            table += f"\n| {total - i} | <img src='{user['avatar_url']}' width='50'> | {user['login']} | {info['starred_at']} |"
+        lines.append(table)
         new_content = '\n\n'.join(lines)
         encoded_content = base64.b64encode(new_content.encode()).decode()
-        sha = res.json()['sha']
         data = {
             "message": f"Update star count to {stars}",
             "content": encoded_content,
